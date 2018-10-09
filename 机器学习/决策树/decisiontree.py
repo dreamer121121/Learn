275eb04d478ee0e15d684f5a9a45c47c070a1bf8
xt <2761564455@qq.com>
2018-10-09 17:25:27 +0800

    10.9

--- 机器学习/决策树/decisiontree.py
+++ 机器学习/决策树/decisiontree.py
@@ -8,15 +8,14 @@ import os
 from sklearn import tree
 from sklearn import datasets
 import graphviz
-os.chdir(r'C:\Users\jack xia\Desktop\连接公司\ARGUS\机器学习\决策树')
-#整理数据
-data=datasets.load_iris()
-train_X=data['data']
-train_y=data['target']
-clf=tree.DecisionTreeClassifier()
-clf=clf.fit(train_X,train_y)
-dot_data=tree.export_graphviz(clf,out_file=None)
-graph=graphviz.Source(dot_data)
-graph.render('iris')
-#result=clf.predict(train_X[:1,:])
-
+# os.chdir(r'C:\Users\jack xia\Desktop\连接公司\ARGUS\机器学习\决策树')
+# 整理数据
+data = datasets.load_iris()
+train_X = data['data']
+train_y = data['target']
+clf = tree.DecisionTreeClassifier()  # 构建分类器
+clf = clf.fit(train_X, train_y)
+dot_data = tree.export_graphviz(clf, out_file=None)
+# graph=graphviz.Source(dot_data)
+# graph.render('iris')
+# result=clf.predict(train_X[:1,:])
