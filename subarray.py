b895a13cb430a8e681d18278ed2c77d00f2ddf0a
xiatao <2761564455@qq.com>
2018-10-11 08:52:48 +0800

    10.11

--- subarray.py
+++ subarray.py
@@ -1,11 +1,12 @@
 import datetime
 import time
 import numpy as np
-start = time.clock()
-print(start)
-a = np.arange(-500, 500)
-max = 0
-trace = []
+
+# start = time.clock()
+# print(start)
+# a = np.arange(-500, 500)
+# max = 0
+# trace = []
 
 # O(n3)
 # for i in range(len(a)):
@@ -35,14 +36,43 @@ trace = []
 
 # 递归法
 
+# 递归法求最大子数组
+def MaxCrossSubArray(A, low, mid, high):
+    LeftMaxSum = A[mid]
+    leftSum = A[mid]
+    leftIndex = mid
+    for i in range(mid - 1, low - 1, -1):
+        leftSum = leftSum + A[i]
+        if leftSum > LeftMaxSum:
+            LeftMaxSum = leftSum
+            leftIndex = i
+    rightMaxSum = 0
+    rightSum = 0
+    rightIndex = mid
+    for i in range(mid + 1, high + 1):
+        rightSum += A[i]
+        if rightSum > rightMaxSum:
+            rightMaxSum = rightSum
+            rightIndex = i
+    MaxSum = LeftMaxSum + rightMaxSum
+    return (MaxSum, leftIndex, rightIndex)
 
-def devide(a):
-    if len(a) == 1:
-        return a
-    middle = len(a) // 2
-    li = devide(a[0:middle])
-    ri = devide(a[middle+1:])
-    return combine(li,ri)
 
-def combine(li,ri):
-    pass
+def MaxSubArray(A, low, high):
+    if low == high:
+        return (A[low], low, high)
+    mid = (low + high) // 2
+    Left = MaxSubArray(A, low, mid)  # 开始递归了一直要将数组分解到单个后才能继续向下进行
+    Cross = MaxCrossSubArray(A, low, mid, high)
+    Right = MaxSubArray(A, mid + 1, high)
+    List = [Left, Cross, Right]
+    result = sorted(List, key=lambda list: list[0], reverse=True)
+    return result[0]
+
+
+a = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+print(MaxSubArray(a, 0, len(a)-1))
+
+
+
+
