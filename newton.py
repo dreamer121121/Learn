44903bc41a7700827824915d2d1ab47bb6c82f1f
xt <2761564455@qq.com>
2018-10-30 13:33:02 +0800

    添加牛顿法

---
+++ newton.py
@@ -0,0 +1,17 @@
+# #coding:utf-8
+# def D(t):
+#     return 3*(t*t)
+#
+#
+# def Newton_3(c, t0):
+#     t=t0
+#     i=1
+#     while abs((t*t*t - c))> 0.001:
+#         print("第"+str(i)+"次迭代")
+#         t = t - (float((t*t*t - c)) / float(D(t)))
+#         print(t)
+#         i+=1
+#     return t
+#
+# print(Newton_3(2, 1))
+print((1.25993349345)**3)
