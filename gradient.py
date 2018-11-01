37cf52bcd3a49c5a369e3dca98952a8771fe5255
xt <2761564455@qq.com>
2018-11-01 10:22:46 +0800

    添加文件

--- gradient.py
+++ gradient.py
@@ -1,3 +1,4 @@
+# conding:utf-8
 rooms = [3, 3, 3, 2, 4]
 price = [400, 330, 369, 232, 540]
 area = [2104, 1600, 2400, 1416, 3000]
@@ -5,38 +6,37 @@ area = [2104, 1600, 2400, 1416, 3000]
 
 def gradientDescent(rooms, price, area):
     theta = [1, 1, 1]
-    # 构造代价函数，开始进行迭代
     iteras = 400
     i = 1
     temp0, temp1, temp2 = theta[0], theta[1], theta[2]
-    while i <= iteras:  # 控制迭代次数
-        print("第%d次迭代"%i)
+    while i <= iteras:
+        print "iteras=:"+str(i)
         sum0 = 0
         sum1 = 0
         sum2 = 0
         # for theta0
-        for m in range(len(rooms)):  # 将样本逐一带入模型
+        for m in range(len(rooms)):
             h_theta = theta[0] + theta[1] * area[m] + theta[2] * rooms[m]
-            y_m = price[m]  # 真实值
+            y_m = price[m]
             sum0 += (h_theta - y_m) * 1
         # for theta1
-        for m in range(len(rooms)):  # 将样本逐一带入模型
+        for m in range(len(rooms)):
             h_theta=theta[0]+theta[1]*area[m]+theta[2]*rooms[m]
-            y_m=price[m]  # 真实值
+            y_m=price[m]
             sum1+=(h_theta-y_m)*area[m]
         # for theta2
-        for m in range(len(rooms)):  # 将样本逐一带入模型
+        for m in range(len(rooms)):
             h_theta = theta[0] + theta[1] * area[m] + theta[2] * rooms[m]
-            y_m = price[m]  # 真实值
+            y_m = price[m]
             sum2+=(h_theta-y_m)*rooms[m]
-        # 计算待更新值
+
         temp0 -= 0.0000001 * 0.2 * sum0
         temp1 -= 0.0000001 * 0.2 * sum1
         temp2 -= 0.0000001 * 0.2 * sum2
-        # 判断是否全部收敛
+
         if abs(temp0 - theta[0]) <= 0.0001 and abs(temp1 - theta[1]) <= 0.0001 and abs(temp2 - theta[2]) <= 0.0001:
             break
-        # 更新参数
+
         theta[0] = temp0
         theta[1] = temp1
         theta[2] = temp2
@@ -46,6 +46,6 @@ def gradientDescent(rooms, price, area):
 
 theta_result = gradientDescent(rooms, price, area)
 print(theta_result)
-# 检测模型
+
 predict = theta_result[0] + theta_result[1] * 2104 + theta_result[2] * 3
 # print(predict)
