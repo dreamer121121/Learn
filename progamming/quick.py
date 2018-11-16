0dffe84d9a1194bf6e98dde46f5df7dded60fed6
dreamer <2761564455@qq.con>
2018-11-16 22:15:21 +0800

    添加快排

---
+++ progamming/quick.py
@@ -0,0 +1,23 @@
+def quick_sort(array, l, r):
+    if l < r:
+        q = partition(array, l, r)
+        quick_sort(array, l, q - 1)
+        quick_sort(array, q + 1, r)
+
+
+def partition(array, l, r):
+    x = array[r]
+    i = l - 1
+    for j in range(l, r):
+        if array[j] <= x:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[r] = array[r], array[i + 1]
+    print(array)
+    return i + 1
+
+
+if __name__ == '__main__':
+    A = [5, 23, 3, 6, 15]
+    quick_sort(A, 0, len(A)-1)
+    print(A)
