95bf23d2698ad6f2e139d8b9be852655ba913526
dreamer <2761564455@qq.con>
2018-11-20 21:31:17 +0800

    分治法解决多项式乘法问题

--- progamming/poly multtation.py
+++ progamming/poly multtation.py
@@ -1,16 +1,18 @@
-a = [1, 2, 3]
-b = [3, 2, 2]
-c = []
+# brute algorithm 算法复杂度为O(n^2)容易误解为O(n^3)
+# a = [1, 2, 3]
+# b = [3, 2, 2]
+# c = []
+# sum = 0
+# count = 0
+# for k in range(len(a + b) - 1):
+#     sum = 0
+#     for i in range(len(a)):
+#         for j in range(len(b)):
+#             if i + j == k:
+#                 sum += a[i] * b[j]
+#                 count += 1
+#     c.append(sum)
+# # print(c)
+# print(count)
 
-sum = 0
-count=0
-for k in range(len(a+b)-1):
-    sum=0
-    for i in range(len(a)):
-        for j in range(len(b)):
-            if i + j == k:
-                sum += a[i] * b[j]
-                count+=1
-    c.append(sum)
-# print(c)
-print(count)
+# Devide and conquer
