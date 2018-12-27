1a4cc05fa61a5244bbe2fccd7caddd9162b66f45
xiatao <2761564455@qq.com>
2018-12-27 20:36:07 +0800

    完成因子分析（因子分析,聚类,城市排行)

--- 因子分析/code/scatter.py
+++ 因子分析/code/scatter.py
@@ -3,44 +3,39 @@ from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
 import matplotlib
+import os
+os.chdir(r'C:\Users\jack xia\Desktop\learn\learn\因子分析')
 
 # 读取数据
-data = pd.read_csv('julei.csv').values
+data = pd.read_csv('4cluster_result.csv', encoding='gb18030').values
 print(data)
 data = data.tolist()
 X = []
 Y = []
-Z = []
 label = []
 for item in data:
     X.append(item[1])
     Y.append(item[2])
-    Z.append(item[3])
-    label.append(item[4])
+    label.append(item[3])
 X = np.array(X)
 Y = np.array(Y)
-Z = np.array(Z)
 print(label)
 
 # 绘图
-font = {'family': 'MicroSoft YaHei',
-        'weight': 'bold',
-        'size': 10}
 matplotlib.rcParams['font.family'] = 'SimHei'
 matplotlib.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
-fig = plt.figure()
-ax = Axes3D(fig)
 for i in range(len(label)):
     if label[i] == 1:
-        ax.scatter(X[i], Y[i], Z[i], color='red')
+        plt.scatter(X[i], Y[i], color='red')
     elif label[i] == 2:
-        ax.scatter(X[i], Y[i], Z[i], color='green')
+        plt.scatter(X[i], Y[i], color='green')
+    elif label[i] == 3:
+        plt.scatter(X[i], Y[i], color='blue')
     else:
-        ax.scatter(X[i], Y[i], Z[i], color='blue')
+        plt.scatter(X[i], Y[i], color='black')
 
-ax.set_xlabel('财政状况和工业水平')
-ax.set_ylabel('城市规模和农业发展水平')
-ax.set_zlabel('居民收入水平')
-ax.set_title('河北省各城市聚类分析结果')
+plt.xlabel('经济发展水平')
+plt.ylabel('居民收入水平')
+plt.title('河北省各城市聚类分析结果')
 plt.savefig('cluster.jpg')
 plt.show()
--- 因子分析/code/score.py
+++ 因子分析/code/score.py
@@ -1,26 +1,32 @@
+import json
 import numpy as np
 import pandas as pd
-#读入数据
-data = pd.read_csv('julei.csv').values
+import os
+
+f = open('paihang.txt', 'w')
+# 读入数据
+data = pd.read_csv('cluster_result.csv', encoding='gb18030').values
 data = data.tolist()
-# 计算城市得分
-contribution1 = 76.866
-contribution2 = 12.608
-contribution3 = 5.845
-total_contribution = 95.319
-w = [0, 0, 0]
+print(data)
+# # 计算城市得分
+contribution1 = 76.246
+contribution2 = 13.654
+total_contribution = 89.9
+w = [0, 0]
 city_list = ['石家庄', '承德市', '张家口市', '秦皇岛市', '唐山市', '廊坊市', '保定市', '沧州市', '衡水市', '邢台市', '邯郸市']
 for i in range(3):
     w[0] = contribution1 / total_contribution
     w[1] = contribution2 / total_contribution
-    w[2] = contribution3 / total_contribution
 scores = {}
 i = 0
 for city in city_list:
-    score = data[i][1] * w[0] + data[i][2] * w[1] + data[i][3] * w[2]
+    score = data[i][1] * w[0] + data[i][2] * w[1]
     scores[city] = score
     i += 1
 # 城市排行
 print(type(scores.items()))  # dict.items()将原字典转换为可迭代对象
 paihang = sorted(scores.items(), key=lambda item: item[1], reverse=True)
-print('----排行----', paihang)
+print(paihang)
+f.write(json.dumps(paihang, ensure_ascii=False) + '\n')
+f.write(json.dumps(w))
+f.close()
