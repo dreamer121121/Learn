70b27461cb1ae67f87c7a9edb8c90a77d17a7dc8
dreamer <2761564455@qq.con>
2019-04-29 21:13:13 +0800

    添加二叉树类

--- progamming/data_structure/bintree.py
+++ progamming/data_structure/bintree.py
@@ -1,29 +1,72 @@
-def BinTree(data,left=None,right=None):
-    return [data,left,right]
+#列表或者元组方式实现二叉树
+# def BinTree(data,left=None,right=None):
+#     return [data,left,right]
+#
+# def is_empty_BinTree(btree):
+#     return btree is None
+#
+# def root(btree):
+#     return btree[0]
+#
+# def left(btree):
+#     return btree[1]
+#
+# def right(btree):
+#     return btree[2]
+#
+# def set_root(btree,data):
+#     btree[0] = data
+#
+# def set_left(btree,left):
+#     btree[1] = left
+#
+# def set_right(btree,right):
+#     btree[2] = right
+#
+# #构建一棵二叉树
+# t1 = BinTree(2,BinTree(5),BinTree(4))
+# set_left(left(t1),BinTree(5))
 
-def is_empty_BinTree(btree):
-    return btree is None
+class BinTNode:
+    def __init__(self,val,left = None,right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def root(btree):
-    return btree[0]
+root = BinTNode(1,BinTNode(2),BinTNode(3))
 
-def left(btree):
-    return btree[1]
 
-def right(btree):
-    return btree[2]
+def pre(root):
+    """
+    前序遍历二叉树
+    :param root:
+    :return:
+    """
+    if root == None:
+        return
+    print("Node："+str(root.val))
+    pre(root.left)
+    pre(root.right)
 
-def set_root(btree,data):
-    btree[0] = data
+def count_BinTNodes(root):
+    """
+    计算二叉树中的节点个数
+    :param root:
+    :return:
+    """
+    if root == None:
+        return 0
+    else:
+         return 1+count_BinTNodes(root.left)+count_BinTNodes(root.right)
 
-def set_left(btree,left):
-    btree[1] = left
-
-def set_right(btree,right):
-    btree[2] = right
-
-#构建一棵二叉树
-t1 = BinTree(2,BinTree(5),BinTree(4))
-set_left(left(t1),BinTree(5))
-
+def sum_BinTNodes(root):
+    """
+    计算二叉树中节点和
+    :param root:
+    :return:
+    """
+    if root is None:
+        return 0
+    else:
+        return root.val + sum_BinTNodes(root.left)+sum_BinTNodes(root.right)
 
